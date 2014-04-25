006eaacf8c6664277796ae10c44f2b07dcc1cff5
Tolga Caner <Tolga@Tolgas-MacBook-Pro.local>
2014-04-25 15:23:20 +0300

    First Blood

--- FourSqurae_TolgaCaner/FSViewController.m
+++ FourSqurae_TolgaCaner/FSViewController.m
@@ -18,6 +18,7 @@
 {
     [super viewDidLoad];
     [[self view] setBackgroundColor:[UIColor redColor]];
+    NSLog(@"yo");
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
