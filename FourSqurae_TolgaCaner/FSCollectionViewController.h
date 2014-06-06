f968d41e5901597e0f250795a8bf77ebb9fd55cc
Tolga Caner <Tolga@Tolgas-MacBook-Pro.local>
2014-06-06 19:22:31 +0300

    Tolga

---
+++ FourSqurae_TolgaCaner/FSCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  FSCollectionViewController.h
+//  FourSqurae_TolgaCaner
+//
+//  Created by Tolga Caner on 02/06/14.
+//  Copyright (c) 2014 Tolga Caner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FSCollectionViewController : UIViewController
+
+@end
---
+++ FourSqurae_TolgaCaner/FSCollectionViewController.m
@@ -0,0 +1,38 @@
+//
+//  FSCollectionViewController.m
+//  FourSqurae_TolgaCaner
+//
+//  Created by Tolga Caner on 02/06/14.
+//  Copyright (c) 2014 Tolga Caner. All rights reserved.
+//
+
+#import "FSCollectionViewController.h"
+
+@interface FSCollectionViewController ()
+
+@end
+
+@implementation FSCollectionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
