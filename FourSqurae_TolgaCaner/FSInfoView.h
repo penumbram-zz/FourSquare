55a206f209c05d003e01af15083ab4018d70c6ad
Tolga Caner <Tolga@Tolgas-MacBook-Pro.local>
2014-06-07 02:26:41 +0300

    3of5requests

---
+++ FourSqurae_TolgaCaner/FSInfoView.h
@@ -0,0 +1,13 @@
+//
+//  FSInfoView.h
+//  FourSqurae_TolgaCaner
+//
+//  Created by Tolga Caner on 06/06/14.
+//  Copyright (c) 2014 Tolga Caner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FSInfoView : UIView
+
+@end
---
+++ FourSqurae_TolgaCaner/FSInfoView.m
@@ -0,0 +1,31 @@
+//
+//  FSInfoView.m
+//  FourSqurae_TolgaCaner
+//
+//  Created by Tolga Caner on 06/06/14.
+//  Copyright (c) 2014 Tolga Caner. All rights reserved.
+//
+
+#import "FSInfoView.h"
+
+@implementation FSInfoView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
